cd0aa24fd4dc49ab7b196e0339c977c50ed78f14
fengchen <chen.feng1@husky.neu.edu>
2020-12-30 14:09:55 -0800

    Initial commit

---
+++ map.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/xml"
+	"io/ioutil"
+	"net/http"
+)
+
+type Sitemapindex struct {
+	Locations []string `xml:"sitemap>loc"`
+}
+
+type News struct {
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+type NewsMap struct {
+	Keyword  string
+	Location string
+}
+
+func main() {
+	var s Sitemapindex
+	var n News
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &s)
+	for _, Location := range s.Locations {
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
+	}
+}
